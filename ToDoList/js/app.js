0c8c19c8e2af14d34b1e2b4e9f95add1b9269c7b
MikeSadler84 <61479491+MikeSadler84@users.noreply.github.com>
2020-08-28 12:20:24 -0500

    Add files via upload
    Rearranged the Jquery to make the button do everything in one function

--- ToDoList/js/app.js
+++ ToDoList/js/app.js
@@ -8,13 +8,6 @@ function addToDo(){
     };
 
     displayToDo(toDo.text);
-    if (text === ""){
-            alert("Text field is empty");   
-            return false;
-    }else{
-        // displayToDo();
-        $("#txt-task").val("");
-    }
     $("#txt-task").focus();
 }
 
@@ -23,22 +16,31 @@ function displayToDo(toDo){
     $("#pending-list").append(li);
 }
 
-function addToFinished(){
-
-    $("#finished-list").append($("#pending-list"));
+// function addToFinished(){
+    
+//     $("#finished-list").append($("#pending-list"));
    
-}
+// }
 
 function init(){
 
 // sense the events here - click
-    $(".btn-primary").click(addToDo);
+    // $(".btn-primary").click(addToDo);
     $("#txt-task").keypress(function(e){
         if(e.key === "Enter"){
             addToDo();
         }
     });
-    $(".btn-danger").click(addToFinished);
+    $(".btn-primary").click(function(){
+    if ($("#txt-task").val() === ""){
+        alert("Text field is empty");   
+        return false;
+    }else{
+        addToDo();
+        $("#txt-task").val("");
+    }
+    });
+    // $(".btn-danger").click(addToFinished);
 
 }
 
